5d567355ab3a36dbe24dce48fa0d0f615635dbf4
vitaoTM <vitor.grosskopf@gmail.com>
2022-10-13 11:07:07 -0300

    adicionando metodos de saque, transferencia e deposito

--- ContaCorrente.cs
+++ ContaCorrente.cs
@@ -15,9 +15,36 @@ namespace bytebank
 
         public void Depositar(double valor)
         {
-            this.
+            this.saldo += valor;
+
         }
 
+        public bool Sacar(double valor)
+        {
+            if(valor <= saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        public bool Transeferir (double valor, ContaCorrente destino)
+        {
+            if (this.saldo <= valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                destino.saldo += valor;
+                return true;
+            }
+        }
         
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,3 +8,15 @@ contaDoVitor.conta = "123456-x"
 contaDoVitor.saldo = 5000.15;
 
 Console.WriteLine($"Saldo do vitor na conta {contaDoVitor.conta}, é de {contaDoVitor.saldo}");
+
+ContaDoVitor.Depositar(1000.20);
+
+Console.WriteLine($"Saldo do vitor na conta {contaDoVitor.conta} pós deposito, é de {contaDoVitor.saldo}");
+
+if(ContaDoVitor.Sacar(500.0) == true)
+{
+    Console.WriteLine($"Saldo do vitor na conta {contaDoVitor.conta} pós saque, é de {contaDoVitor.saldo}");
+}
+else{
+    Console.WriteLine("Não é póssivel realizar o saque");
+};
